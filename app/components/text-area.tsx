c60c61b23105e7ec2aa613d58a3c7792bdead607
wendel kaua <wendellkauan870@gmail.com>
2026-05-06 17:31:05 -0300

    production

--- app/components/text-area.tsx
+++ app/components/text-area.tsx
@@ -8,7 +8,7 @@ export function TextArea(
       {...props}
       className={cn(
         `w-wfull p-3 bg-zinc-900 text-white placeholder:text-zinc-500 rounded-xl
-        border border-transparent hover:border-zinc-900 hover:text-zinc-200 active:border-zinc-400`,
+        border border-transparent hover:border-zinc-900 hover:text-zinc-200 active:border-zinc-300`,
         props.className,
       )}
     />
